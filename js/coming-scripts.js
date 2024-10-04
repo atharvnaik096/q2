2d69eca3d709696c7cc900b8a1251d1610321e49
atharvnaik096 <136322603+atharvnaik096@users.noreply.github.com>
2024-10-04 23:06:55 +0530

    by atharv naik

---
+++ js/coming-scripts.js
@@ -0,0 +1,13 @@
+
+  document.getElementById('notify-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    
+    const email = document.getElementById('email').value;
+    
+        alert('Thank you for your message! We will get back to you soon.');
+        // Here, you can send the form data to a backend service via AJAX or similar method.
+        this.reset(); 
+   
+        
+});
